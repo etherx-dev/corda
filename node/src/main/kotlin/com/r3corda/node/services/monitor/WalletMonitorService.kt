8b577be1638bbade1cc651afb10d5a7cf77a7970
Andras Slemmer <andras.slemmer@r3cev.com>
2016-09-12 10:42:25 +0100

    client, node: Handle snapshot in explorer

--- node/src/main/kotlin/com/r3corda/node/services/monitor/WalletMonitorService.kt
+++ node/src/main/kotlin/com/r3corda/node/services/monitor/WalletMonitorService.kt
@@ -137,7 +137,7 @@ class WalletMonitorService(net: MessagingService, val smm: StateMachineManager, 
     fun processRegisterRequest(req: RegisterRequest) {
         try {
             listeners.add(RegisteredListener(req.replyToRecipient, req.sessionID))
-            val stateMessage = StateSnapshotMessage(services.walletService.currentWallet.states.map { it.state.data }.toList(),
+            val stateMessage = StateSnapshotMessage(services.walletService.currentWallet.states.toList(),
                     smm.allStateMachines.map { it.javaClass.name })
             net.send(net.createMessage(STATE_TOPIC, DEFAULT_SESSION_ID, stateMessage.serialize().bits), req.replyToRecipient)
 
